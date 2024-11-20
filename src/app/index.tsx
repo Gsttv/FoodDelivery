794da033fcddc8304ac9f358e07ec986784792ec
Gustavo Morais <gustvmorais@gmail.com>
2024-11-19 22:17:40 -0300

    Components Iniciais

---
+++ src/app/index.tsx
@@ -0,0 +1,38 @@
+import { View, ScrollView } from "react-native";
+import { Header } from "../components/header";
+
+import Constants from 'expo-constants'
+import { Banner } from "../components/banner";
+import { Search } from "../components/search";
+import { Section } from "../components/section";
+import { TrendingFoods } from "../components/trending";
+
+const statusBarHeight = Constants.statusBarHeight;
+
+export default function Index() {
+  return (
+      <ScrollView style = {{ flex: 1 }} className="bg-slate-200"
+      showsVerticalScrollIndicator={false}>
+
+        <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8 }}>
+          <Header/>
+
+          <Banner/>
+
+          <Search/>
+        </View>
+
+
+        <Section
+        name="Comidas em alta"
+        label="Veja mais"
+        action={ () => console.log("CLICOU NO VEJA MAIS")}
+        size="text-2xl"
+        />
+
+        <TrendingFoods/>
+
+      </ScrollView>
+
+  );
+}
